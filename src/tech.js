e6fa8056e81ca843ae1983c6630f54d14924a1ec
CATTOLICA\PelliniG <giovanni.pellini@cattolicaits.it>
2022-06-06 17:13:02 +0200

    Added NUnit to VPT

--- src/tech.js
+++ src/tech.js
@@ -22,7 +22,8 @@ class Tech extends Component {
         if (this.props.title.includes('C#')) imgPath = "/img/Icon/c-sharp.png"
         if (this.props.title.includes('Bootstrap')) imgPath = "/img/Icon/BootStrap.png"
         if (this.props.title.includes('Github')) imgPath = "/img/Icon/github.svg"
-
+        if (this.props.title.includes('NUnit')) imgPath = "/img/Icon/NUnit.svg"
+        
         if (imgPath.length > 0)
             return (
                 <>
